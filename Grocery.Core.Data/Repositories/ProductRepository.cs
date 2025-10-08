d3b8ad1971c1101cff7fb54bb7c24083b989b903
Runar <s1208862@student.windesheim.nl>
2025-10-08 16:36:26 +0200

    Finished UC14. Added a price to the product class. Added prices to each product in the productrepository. Added the price onto the product screen and the grocerylistitem screen.

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -6,14 +6,16 @@ namespace Grocery.Core.Data.Repositories
     public class ProductRepository : IProductRepository
     {
         private readonly List<Product> products;
+
         public ProductRepository()
         {
             products = [
-                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25)),
-                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30)),
-                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12)),
-                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31))];
+                new Product(1, "Melk", 300, new DateOnly(2025, 9, 25), 2.50),
+                new Product(2, "Kaas", 100, new DateOnly(2025, 9, 30), 3.00),
+                new Product(3, "Brood", 400, new DateOnly(2025, 9, 12), 1.00),
+                new Product(4, "Cornflakes", 0, new DateOnly(2025, 12, 31), 1.75)];
         }
+
         public List<Product> GetAll()
         {
             return products;
